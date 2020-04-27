182c382493282934a33ed750c9e4374c011ae837
Guston70 <agustonny1970@gmail.com>
2020-04-28 03:31:50 +0700

    menambah judul dan paragraf di index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>coba git remote 2</title>
+</head>
+
+<body>
+    <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita fugiat asperiores illo vero, earum neque odio ipsam quidem vitae tempora cum consequuntur repellat distinctio quibusdam. Exercitationem eligendi itaque repellendus assumenda.</p>
+
+</body>
+
+</html>
